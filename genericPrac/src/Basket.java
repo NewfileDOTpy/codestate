5602089e0c3b14e85d309804a743e8aba28145a5
Gomuang <97213554+NewfileDOTpy@users.noreply.github.com>
2023-09-07 15:57:22 +0900

    Add files via upload

---
+++ genericPrac/src/Basket.java
@@ -0,0 +1,24 @@
+public class Basket<T> {
+
+    private T item;
+
+    public Basket(T item) {
+        this.item = item;
+    }
+
+    public T getItem() {
+        return item;
+    }
+
+    public void setItem(T item) {
+        this.item = item;
+    }
+
+
+    public <T> void baketAdd(T num1, T num2){
+        T sum = num1;
+
+    }
+}
+
+
